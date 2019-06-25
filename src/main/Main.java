f5632477504d07284dfe4b01b1279efa9403b99d
Fabián Camilo Ordóñez <fcordonezo@unal.edu.co>
2019-06-25 01:18:47 -0500

    removed print line

--- src/main/Main.java
+++ src/main/Main.java
@@ -28,6 +28,5 @@ public class Main {
         Processor processor = Processor.getProcessor();
         Compiler.codeToBits();
         processor.process();
-        System.out.println(processor.getRegisters().getRegistersHash().get("A"));
     }
 }
